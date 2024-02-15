dc7f68c2c47e1cd314568dc4086189fff0e3323c
Cafe Soluble <jeanchri.garcia@gmail.com>
2024-02-15 22:51:46 +0100

    MAJ routes

--- routes.py
+++ routes.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    # Obtenir la date actuelle
-    today = datetime.date.today()
-    return render_template("index.html", today=today)
+
+    return render_template("index.html")
